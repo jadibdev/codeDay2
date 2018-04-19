de23a256b2fb77573891ce4b1e4252eaef24f8d9
mac <mac@MacBook-Pro-de-mac-2.local>
2018-04-19 17:56:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var city = document.getElementById('sf');
+
+city.addEventListener("click", function() {
+  alert("San Francisco (initials SF[17]; /ˌsæn frənˈsɪskoʊ, fræn-/, Spanish for 'Saint Francis'; Spanish: [san franˈsisko]), officially the City and County of San Francisco, is the cultural, commercial, and financial center of Northern California. It covers an area of about 46.89 square miles (121.4 km2),[18] mostly at the north ");
+});
+
+/*var name = "Mohamed Jadib";
+var age = 37;
+var present = true;
+
+if (present === true) {
+  alert("this is true");
+}*/
